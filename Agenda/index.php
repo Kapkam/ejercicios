12633aa9421f4505abf0b344115d0980c1c48547
Kapkam <adrinro@gmail.com>
2019-10-08 21:40:36 +0200

    Ejercicio agenda

---
+++ Agenda/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>SE HA INCUIDO LA AGENDA!</h1>
+    <?php
+      include "agenda.php";
+     ?>
+  </body>
+</html>
